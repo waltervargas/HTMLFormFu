11aefb55e02bb4332264ac6a4d78c2ed483bb92d
Walter Vargas <walter@covetel.com.ve>
2011-05-15 17:42:23 -0430

    slskfj

--- js/validate_formfu.js
+++ js/validate_formfu.js
@@ -1,16 +1,36 @@
 /* 
- * Extendiendo jquery con funciones propias
- */
+Archivo: 	Covetel.Formularios.js
+Autor: 		Walter Vargas <walter@covetel.com.ve>
+Descripción: Esta libreria se encarga de gestionar los formularios. 
+*/
 
-// Obtiene los valores de los checkboxes seleccionados
-//
-jQuery.fn.getCheckboxValues = function(){
-    var values = [];
-    var i = 0;
-    this.each(function(){
-        // guarda los valores en un array
-        values[i++] = $(this).val();
-    });
-    // devuelve un array con los checkboxes seleccionados
-    return values;
-} 
+(function($){
+ $.fn.campo_requerido = function(opciones){
+
+	// Opciones 
+	var defaults = {
+		mensaje: 'Campo Obligatorio',
+		asterisk: true,
+	};
+	
+	var opciones = $.extend(defaults, opciones);	
+	
+	return this.each(function(){
+		obj = $(this);
+
+		// limpieza primero 
+        obj.children(".requerido").remove();
+
+		if (opciones.asterisk){
+			obj.prepend('<span class="requerido"> * </span>');
+		}
+		obj.append('<div class="requerido"> '+opciones.mensaje+' </div>');
+	});
+ };
+})(jQuery);
+
+// Implementando la libreria.
+
+$(document).ready(function(){
+	$("div.constraint_required").campo_requerido();	
+});
